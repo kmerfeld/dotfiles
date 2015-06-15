25eba2f5fd0a62e3d11ad69390211cc7bb02e1f9
root <root@ldap.(none)>
2015-06-15 15:17:00 -0500

    Fixed spelling error and removed covim

--- install.sh
+++ install.sh
@@ -36,9 +36,8 @@ git clone https://github.com/nanotech/jellybeans.vim.git ~/dotfiles/vim/bundle/j
 git clone https://github.com/scrooloose/nerdtree.git ~/dotfiles/vim/bundle/nerdtree
 git clone https://github.com/ervandew/supertab.git ~/dotfiles/vim/bundle/supertab
 git clone https://github.com/terryma/vim-expand-region.git ~/dotfiles/vim/bundle/vim-expand-region
-git clone git://github.com/FredKSchott/CoVim.git ~/dotfiles/vim/bundle/CoVim
 git clone https://github.com/tpope/vim-fugitive.git ~/dotfiles/vim/bundle/fugitive
 git clone https://github.com/tpope/vim-vividchalk.git ~/dotfiles/vim/bundle/vividchalk
-git clone https://guthib.com/scrooloose/syntastic.git ~/dotfiles/vim/bundle/syntastic
+git clone https://githib.com/scrooloose/syntastic.git ~/dotfiles/vim/bundle/syntastic
 git clone https://github.com/morhetz/gruvbox.git ~/dotfiles/vim/bundle/gruvbox
 
