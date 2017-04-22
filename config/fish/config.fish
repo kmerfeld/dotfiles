a8ea6d732473b68d45d4e272d5d51337cf104ed8
kyle merfeld <kmerfeld1@gmail.com>
2017-04-22 11:19:54 -0500

    adds some env variables for tiny-care-terminal

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,3 +6,12 @@ alias nano /usr/bin/vim
 alias irc "mosh home -- tmux a -t IRC"
 
 set -gx PATH $HOME/dotfiles/scripts $PATH
+
+set VISUAL nvim
+set EDITOR "$VISUAL"
+
+set -gx key value
+set -gx TTC_WEATHER Fargo
+set -gx TTC_CELSIUS false
+set -gx TTC_APIKEYS false
+set -gx TTC_REPOS dotfiles
